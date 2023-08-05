fb7d4836f37a319823c0fb185ab79731f844c5dc
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-08-05 19:43:00 +0500

    Repository pattern has been added

---
+++ Services/Interfaces/IWeatherForecast.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiRepository.Services.Interfaces
+{
+    public interface IWeatherForecast
+    {
+        // inteface is like contact, whosoever will inherit interface needs to implement the  
+        IEnumerable<WeatherForecast> Get();
+    }
+}
---
+++ Services/WeatherForecast.cs
@@ -0,0 +1,8 @@
+﻿using WebApiRepository.Services.Interfaces;
+
+namespace WebApiRepository.Services
+{
+    public class WeatherForecast: IWeatherForecast
+    {
+    }
+}
